3b6ccd98fa395bed23e250b414e7529fb6d609e8
Liam Cavens <liamcavens@googlemail.com>
2018-06-20 20:59:08 +0100

    fixed more css

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -181,13 +181,15 @@ CountryView.prototype.addToBucketList = function(country) {
 
 CountryView.prototype.render = function(country){
 
+    const listDiv = document.querySelector('#to-see-list');
     const ul = document.querySelector('#countries-list');
     const li = document.createElement('li');
     const PTag = document.createElement('p');
     PTag.innerText = `${country.name} - "${country.capital}"`;
-    PTag.src = country.flag
+    PTag.src = country.flag;
     li.appendChild(PTag);
     ul.appendChild(li);
+    listDiv.appendChild(ul);
 }
 
 
