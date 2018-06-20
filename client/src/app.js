4a9a9a9e6edeec172a16608166bd29cd4802842f
Liam Cavens <liamcavens@googlemail.com>
2018-06-20 16:31:42 +0100

    worked on some app.js

--- client/src/app.js
+++ client/src/app.js
@@ -4,7 +4,34 @@ const Request = require('./services/request.js');
 const request = new Request('http://localhost:3000/buckit');
 
 const appStart = function(){
+  const url = "https://restcountries.eu/rest/v2/all";
+  makeRequest(url, requestCompleteCountry);
 
-console.log("Hello");
+  console.log("Hello");
 }
-window.addEventListener('DOMContentLoaded', appStart);
+
+const makeRequest = function (url, callbackFunction) {
+  const request = new XMLHttpRequest();
+  request.open("GET", this.url);
+  request.addEventListener("load", callbackFunction);
+  request.send();
+}
+
+const requestCompleteCountry = function(){
+  if (this.status !== 200) return;
+
+  const countries = JSON.parse(this.response);
+  populateList(countries);
+}
+
+const populateList = function(countries) {
+  const select = document.querySelector('#country-list');
+  countries.forEach(function(country){
+    const option = document.createElement('option');
+    option.textContent = `${country.name}`;
+    option.value = JSON.stringify(country);
+    select.appendChild(option);
+  });
+};
+
+window.addEventListener('load', appStart);
